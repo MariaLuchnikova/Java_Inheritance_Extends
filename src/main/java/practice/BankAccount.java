2508f27aa261a1c766ab5e305feadc637c569b17
Mariia Luchnikova <luchniklav@yandex.ru>
2022-01-12 21:57:23 +0800

    add Java Inheritance Extends

--- src/main/java/practice/BankAccount.java
+++ src/main/java/practice/BankAccount.java
@@ -1,20 +1,58 @@
 package practice;
 
 public class BankAccount {
+    public double amount;
+    public boolean isProgress;
 
-  public double getAmount() {
-    //TODO: реализуйте метод и удалите todo
-    // верните значение количества денег не счету
-    return 0;
-  }
+    public static void main(String[] args) {
+        BankAccount bankAccount = new BankAccount();
+        CardAccount cardAccount = new CardAccount();
+        DepositAccount depositAccount = new DepositAccount();
+        // cardAccount.setAmount(100);
+        // cardAccount.send(depositAccount, 20);
+    }
 
-  public void put(double amountToPut) {
-    //TODO: реализуйте метод и удалите todo
-    // метод зачисляет деньги на счет
-  }
+    // по-хорошему ещё нужна информация о типе счета, но тогда тесты не пропускают
+    public double getAmount() {
+        System.out.println("Текущий баланс на счете: " + amount);
+        return amount;
+    }
 
-  public void take(double amountToTake) {
-    //TODO: реализуйте метод и удалите todo
-    // метод списывает деньги со счета
-  }
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public void put(double amountToPut) {
+        if (amountToPut > 0) {
+            amount += amountToPut;
+            System.out.println("Баланс на счете после операции пополнения: " + amount);
+        } else {
+            System.out.println("Указана некорректная сумма пополнения.");
+        }
+    }
+
+    public void take(double amountToTake) {
+        if (amountToTake > 0) {
+            if (getAmount() < amountToTake) {
+                System.out.println("На счете недостаточно средств для списания.");
+                isProgress = false;
+            } else {
+                amount -= amountToTake;
+                isProgress = true;
+                System.out.println("Баланс на счете после операции списания: " + amount);
+            }
+        } else {
+            System.out.println("Указана некорректная сумма списания.");
+        }
+    }
+
+    public boolean send(BankAccount receiver, double amount) {
+        take(amount);
+        if (isProgress) {
+            receiver.put(amount);
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
--- src/main/java/practice/CardAccount.java
+++ src/main/java/practice/CardAccount.java
@@ -1,6 +1,26 @@
 package practice;
 
 public class CardAccount extends BankAccount {
-    // не забывайте, обращаться к методам и конструкторам родителя
-    // необходимо используя super, например, super.put(10D);
+    private static final double PERCENT_COMMISSIONS = 0.01;
+
+    @Override
+    public double getAmount() {
+        return super.getAmount();
+    }
+
+    @Override
+    public void setAmount(double amount) {
+        super.setAmount(amount);
+    }
+
+    @Override
+    public void put(double amountToPut) {
+        super.put(amountToPut);
+    }
+
+    @Override
+    public void take(double amountToTake) {
+        amountToTake += (amountToTake * PERCENT_COMMISSIONS);
+        super.take(amountToTake);
+    }
 }
